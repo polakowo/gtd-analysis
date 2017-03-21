afc0a5930e355f463ed80046cd61abbdc9ed1b6f
Oleg Polakow <olegpolakow@Olegs-Air.fritz.box>
2017-03-21 20:36:17 +0100

    New func

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -1,13 +1,17 @@
 function generateRandomData(number) {
     maxValue = 300;
     ds = [];
-    for (var i = 0; i < number; i++) {
-        ds.push(Math.round(Math.random() * maxValue));
-    }
+	for (var i = 0; i < number; i++) {
+		ds.push({
+            key: i,
+            value: Math.round(Math.random() * maxValue)
+        });
+	}
     return ds;
 }
 
-dataset = generateRandomData(20);
+numberOfBars = 20;
+dataset = generateRandomData(numberOfBars);
 
 w = 1000;
 h = 300;
@@ -23,23 +27,29 @@ var xScale = d3.scaleBand() // Divides range into n bands, perfect for ordinal v
     .paddingInner(0.05)
     .paddingOuter(0.1);
 var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset)])
+    .domain([0, d3.max(dataset, function(d) {
+        return d.value;
+    })])
     .rangeRound([0, h]);
+
+var key = function(d) { // key function to bind data
+    return d.key;
+};
 
 // Append and set general attributes
 // Initially we work only on X, and put visualization on Y (bars will fall from sky)
 svg.selectAll("rect")
-    .data(dataset) // capture references to all bars
+    .data(dataset, key) // capture references to all bars
     .enter() // returns elements that do not yet exist
     .append("rect")
     .attr("fill", "teal")
-	.attr("width", xScale.bandwidth())
-	.attr("x", function(d, i) {
-		return xScale(i);
-	});
+    .attr("width", xScale.bandwidth())
+    .attr("x", function(d, i) {
+        return xScale(i);
+    });
 
 svg.selectAll("text")
-    .data(dataset)
+    .data(dataset, key)
     .enter()
     .append("text")
     .attr("font-family", "sans-serif")
@@ -47,16 +57,16 @@ svg.selectAll("text")
     .attr("font-weight", "bold")
     .attr("fill", "white")
     .attr("text-anchor", "middle")
-	.attr("x", function(d, i) {
-		return xScale(i) + xScale.bandwidth() / 2;
-	});
+    .attr("x", function(d, i) {
+        return xScale(i) + xScale.bandwidth() / 2;
+    });
 
 function updateX() {
 
-	xScale.domain(d3.range(dataset.length));
+    xScale.domain(d3.range(dataset.length));
 
-	svg.selectAll("rect")
-        .data(dataset) // work on all bars
+    svg.selectAll("rect")
+        .data(dataset, key) // work on all bars
         .transition()
         .duration(500)
         .attr("width", xScale.bandwidth())
@@ -65,7 +75,7 @@ function updateX() {
         });
 
     svg.selectAll("text")
-        .data(dataset)
+        .data(dataset, key)
         .transition()
         .duration(500)
         .attr("x", function(d, i) {
@@ -75,10 +85,12 @@ function updateX() {
 
 function updateY() {
 
-    yScale.domain([0, d3.max(dataset)]);
+    yScale.domain([0, d3.max(dataset, function(d) {
+        return d.value;
+    })]);
 
     svg.selectAll("rect")
-        .data(dataset)
+        .data(dataset, key)
         .transition()
         .delay(function(d, i) {
             return i / dataset.length * 1000;
@@ -86,17 +98,17 @@ function updateY() {
         .duration(500)
         .ease(d3.easeCubic)
         .attr("y", function(d) { // change values, but not array length
-            return h - yScale(d);
+            return h - yScale(d.value);
         })
         .attr("height", function(d) {
-            return yScale(d);
+            return yScale(d.value);
         })
         .attr("opacity", function(d) {
-            return yScale(d) / h;
+            return yScale(d.value) / h;
         });
 
     svg.selectAll("text")
-        .data(dataset)
+        .data(dataset, key)
         .transition()
         .delay(function(d, i) {
             return i / dataset.length * 1000;
@@ -104,10 +116,10 @@ function updateY() {
         .duration(500)
         .ease(d3.easeCubic)
         .text(function(d) {
-            return d;
+            return d.value;
         })
         .attr("y", function(d) {
-            return h - yScale(d) + 20;
+            return h - yScale(d.value) + 20;
         });
 }
 
@@ -115,38 +127,47 @@ function updateY() {
 updateY(); // here comes visualization of Y
 
 d3.select("#button-random").on("click", function() {
-    dataset = generateRandomData(dataset.length);
+	for (var i = 0; i < dataset.length; i++) {
+		dataset[i].value = Math.round(Math.random() * maxValue);
+	}
 
     updateY();
 });
 
 d3.select("#button-add-bar").on("click", function() {
-	var maxValue = 300;
-    dataset.push(Math.round(Math.random() * maxValue));
+    var maxValue = 300;
+    dataset.push({
+        key: d3.max(dataset, function(d) {
+            return d.key;
+        }) + 1,
+        value: Math.round(Math.random() * maxValue)
+    });
 
     xScale.domain(d3.range(dataset.length));
-    yScale.domain([0, d3.max(dataset)]);
+    yScale.domain([0, d3.max(dataset, function(d) {
+        return d.value;
+    })]);
 
     // Change attributes according to values in dataset
     svg.selectAll("rect")
-        .data(dataset)
+        .data(dataset, key)
         .enter() // work on added bar only
         .append("rect")
         .attr("x", w) // put outside of svg for nice transition from outside
         .attr("width", xScale.bandwidth())
         .attr("fill", "teal")
         .attr("y", function(d) { // change values, but not array length
-            return h - yScale(d);
+            return h - yScale(d.value);
         })
         .attr("height", function(d) {
-            return yScale(d);
+            return yScale(d.value);
         })
         .attr("opacity", function(d) {
-            return yScale(d) / h;
+            return yScale(d.value) / h;
         });
 
     svg.selectAll("text")
-        .data(dataset)
+        .data(dataset, key)
         .enter()
         .append("text")
         .attr("font-family", "sans-serif")
@@ -156,33 +177,33 @@ d3.select("#button-add-bar").on("click", function() {
         .attr("text-anchor", "middle")
         .attr("x", w + xScale.bandwidth() / 2)
         .text(function(d) {
-            return d;
+            return d.value;
         })
         .attr("y", function(d) {
-            return h - yScale(d) + 20;
+            return h - yScale(d.value) + 20;
         });
 
-	updateX();
+    updateX();
 });
 
 d3.select("#button-remove-bar").on("click", function() {
-	dataset.shift();
+    dataset.shift();
 
     svg.selectAll("rect")
-        .data(dataset)
+        .data(dataset, key)
         .exit() // working with those which exits the dataset (analogy to enter)
-		.transition()
+        .transition()
         .duration(500)
-        .attr("x", w)
+        .attr("x", -xScale.bandwidth())
         .remove();
 
     svg.selectAll("text")
-        .data(dataset)
+        .data(dataset, key)
         .exit()
-		.transition()
+        .transition()
         .duration(500)
-        .attr("x", w + xScale.bandwidth() / 2)
+        .attr("x", -xScale.bandwidth() + xScale.bandwidth() / 2)
         .remove();
 
-	updateX();
+    updateX();
 });
